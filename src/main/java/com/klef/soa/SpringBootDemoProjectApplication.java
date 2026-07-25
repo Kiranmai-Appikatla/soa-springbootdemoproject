cde9fdab1342020f4c5bfd2c0506a5f38bd68c70
Kiranmai-Appikatla <2400031871@kluniversity.in>
2026-07-25 08:49:47 +0530

    project v2.0

--- src/main/java/com/klef/soa/SpringBootDemoProjectApplication.java
+++ src/main/java/com/klef/soa/SpringBootDemoProjectApplication.java
@@ -1,9 +1,7 @@
 package com.klef.soa;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;	
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import jdk.internal.org.jline.terminal.TerminalBuilder.SystemOutput;
 
 @SpringBootApplication
 public class SpringBootDemoProjectApplication {
